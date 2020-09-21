f6fac339f3724243df4e1b7358280d5b5eabc20a
Artyom Okhota <shpager@gmail.com>
2020-09-21 20:28:49 +0300

    Initial commit

---
+++ DoubleLinkedList/ListNode.cs
@@ -0,0 +1,10 @@
+﻿namespace DoubleLinkedList
+{
+     public class ListNode
+    {
+        public ListNode Prev { get; set; }
+        public ListNode Next { get; set; }
+        public ListNode Rand { get; set; }
+        public string Data { get; set; }
+    }
+}
---
+++ DoubleLinkedList/ListRand.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace DoubleLinkedList
+{
+    public class ListRand : IEnumerable
+    {
+        public ListNode Head;
+        public ListNode Tail;
+        public int Count;
+
+        public ListRand()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
+
+        public void Serialize(BinaryWriter s)
+        {
+            s.Write(Count);
+            
+            int currentNodeIndex = 0;
+
+            ListNode currentNode = Head;
+            while (currentNode != null)
+            {
+                var randNode = Head;
+
+                // Calculate each node random index
+                var randomNodeIndex = 0;
+                while (randNode != currentNode.Rand)
+                {
+                    randomNodeIndex++;
+                    randNode = randNode.Next;
+                }
+                // Write fields to binary file
+                s.Write(randomNodeIndex);
+                s.Write(currentNode.Data);
+                s.Write(currentNodeIndex);
+
+                currentNodeIndex++;
+                currentNode = currentNode.Next;
+            }
+        }
+        
+        public void Deserealize(BinaryReader s)
+        {
+            
+            int[] randomIndexOfNode = new int[s.ReadInt32()];
+            
+            // Read data form binary file and node
+            while (s.PeekChar() > -1)
+            {
+                int rand = s.ReadInt32();
+                string data = s.ReadString();
+                int cur = s.ReadInt32();
+
+                randomIndexOfNode[cur] = rand;
+
+                Add(data);
+            }
+
+            // Recover random links of each node
+            ListNode currentNode = Head;
+            int index = 0;
+            while (currentNode != null)
+            {
+                var randomNode = Head;
+                for (int i = 0; i < randomIndexOfNode[index]; i++)
+                {
+                    randomNode = randomNode.Next;
+                }
+
+                index++;
+                currentNode.Rand = randomNode;
+                currentNode = currentNode.Next;
+            }
+        }
+        
+        public void Add(string data)
+        {
+            ListNode newNode = new ListNode();
+
+            if (Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                Tail.Next = newNode;
+                newNode.Prev = Tail;
+            }
+
+            Tail = newNode;
+            Tail.Data = data;
+            Count++;
+        }
+
+        /// <summary>
+        /// Set random link to each node in list
+        /// </summary>
+        public void SetRandNodes()
+        {
+            Random rand = new Random();
+            
+            ListNode current = Head;
+            while (current != null)
+            {
+                var randNode = Head;
+                int itemIndex = rand.Next(0, Count);
+                for (int i = 0; i < itemIndex; i++)
+                {
+                    randNode = randNode.Next;
+                }
+
+                current.Rand = randNode;
+                current = current.Next;
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            ListNode current = Head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+    }
+}
